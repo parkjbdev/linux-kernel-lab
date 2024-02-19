26902824ee528ab2c5c4dcceaa330adbef1719e4
parkjbdev <dev@parkjb.com>
2024-02-19 15:20:37 +0900

    feat: complete 04_interrupts.. test needed

--- 04_interrupts/kbd.c
+++ 04_interrupts/kbd.c
@@ -13,22 +13,25 @@ MODULE_DESCRIPTION("KBD");
 MODULE_AUTHOR("Kernel Hacker");
 MODULE_LICENSE("GPL");
 
-#define MODULE_NAME		"kbd"
+#define MODULE_NAME "kbd"
 
-#define KBD_MAJOR		42
-#define KBD_MINOR		0
-#define KBD_NR_MINORS	1
+#define KBD_MAJOR 42
+#define KBD_MINOR 0
+#define KBD_NR_MINORS 1
 
-#define I8042_KBD_IRQ		1
-#define I8042_STATUS_REG	0x64
-#define I8042_DATA_REG		0x60
+#define I8042_KBD_IRQ 1
+#define I8042_STAT_REG 0x65
+#define I8042_DATA_REG 0x61
+#define I8042_STAT_REG_SIZE 1
+#define I8042_DATA_REG_SIZE 1
 
-#define BUFFER_SIZE		1024
-#define SCANCODE_RELEASED_MASK	0x80
+#define BUFFER_SIZE 1024
+#define SCANCODE_RELEASED_MASK 0x80
 
 struct kbd {
 	struct cdev cdev;
 	/* TODO 3: add spinlock */
+	spinlock_t lock;
 	char buf[BUFFER_SIZE];
 	size_t put_idx, get_idx, count;
 } devs[1];
@@ -69,25 +72,37 @@ static int get_ascii(unsigned int scancode)
 	return '?';
 }
 
-static void put_char(struct kbd *data, char c)
+static bool put_char(struct kbd *data, char c)
 {
 	if (data->count >= BUFFER_SIZE)
-		return;
+		return false;
 
 	data->buf[data->put_idx] = c;
 	data->put_idx = (data->put_idx + 1) % BUFFER_SIZE;
 	data->count++;
+
+  return true;
 }
 
 static bool get_char(char *c, struct kbd *data)
 {
 	/* TODO 4: get char from buffer; update count and get_idx */
-	return false;
+	if (data->count == 0)
+		return false;
+
+	*c = data->buf[data->get_idx];
+	data->get_idx = (data->get_idx + 1) % BUFFER_SIZE;
+	data->count--;
+
+	return true;
 }
 
 static void reset_buffer(struct kbd *data)
 {
 	/* TODO 5: reset count, put_idx, get_idx */
+	data->put_idx = 0;
+	data->get_idx = 0;
+	data->count = 0;
 }
 
 /*
@@ -95,16 +110,36 @@ static void reset_buffer(struct kbd *data)
  */
 static inline u8 i8042_read_data(void)
 {
-	u8 val;
+	u8 val = 0;
 	/* TODO 3: Read DATA register (8 bits). */
+	val = inb(I8042_DATA_REG);
 	return val;
 }
 
 /* TODO 2: implement interrupt handler */
+irqreturn_t kbd_interrupt_handler(int irq_no, void *dev_id)
+{
+	irqreturn_t ret = IRQ_NONE;
+
 	/* TODO 3: read the scancode */
+	u8 scancode = i8042_read_data();
 	/* TODO 3: interpret the scancode */
+	int pressed = is_key_press(scancode);
+	int ch = get_ascii(scancode);
+
 	/* TODO 3: display information about the keystrokes */
+	pr_info("IRQ: %d, scancode = 0x%x (%u, %c)\n", irq_no, scancode,
+		scancode, scancode);
+	pr_info("pressed = %d, ch = %c\n", pressed, ch);
+
 	/* TODO 3: store ASCII key to buffer */
+	struct kbd *data = (struct kbd *)dev_id;
+	spin_lock(&data->lock);
+	put_char(data, ch);
+	spin_unlock(&data->lock);
+
+	return ret;
+}
 
 static int kbd_open(struct inode *inode, struct file *file)
 {
@@ -122,14 +157,40 @@ static int kbd_release(struct inode *inode, struct file *file)
 }
 
 /* TODO 5: add write operation and reset the buffer */
-
-static ssize_t kbd_read(struct file *file,  char __user *user_buffer,
+static ssize_t kbd_read(struct file *file, char __user *user_buffer,
 			size_t size, loff_t *offset)
 {
-	struct kbd *data = (struct kbd *) file->private_data;
+	pr_info("read called");
+
+	struct kbd *data = (struct kbd *)file->private_data;
 	size_t read = 0;
 	/* TODO 4: read data from buffer */
+	char c;
+	unsigned long flags;
+
+	spin_lock_irqsave(&data->lock, flags);
+	get_char(&c, data);
+	spin_unlock_irqrestore(&data->lock, flags);
+
+	put_user(c, user_buffer);
+
 	return read;
+}
+
+static ssize_t kbd_write(struct file *file, const char __user *user_buffer,
+			 size_t size, loff_t *offset)
+{
+	pr_info("write called");
+
+  struct kbd* data = file->private_data;
+
+  unsigned long copy_size = copy_from_user(data->buf, user_buffer, size);
+  if (!copy_size) 
+    return -EFAULT;
+
+  *offset += size;
+
+	return size;
 }
 
 static const struct file_operations kbd_fops = {
@@ -138,36 +199,62 @@ static const struct file_operations kbd_fops = {
 	.release = kbd_release,
 	.read = kbd_read,
 	/* TODO 5: add write operation */
+	.write = kbd_write,
 };
 
 static int kbd_init(void)
 {
-	int err;
+	dev_t dev = MKDEV(KBD_MAJOR, KBD_MINOR);
+	int err = 0;
 
-	err = register_chrdev_region(MKDEV(KBD_MAJOR, KBD_MINOR),
-				     KBD_NR_MINORS, MODULE_NAME);
+	err = register_chrdev_region(dev, KBD_NR_MINORS, MODULE_NAME);
+
 	if (err != 0) {
-		pr_err("register_region failed: %d\n", err);
+		pr_err("[Fail] register_region returned %d\n", err);
 		goto out;
 	}
 
+	pr_info("[Success] register_chrdev_region\n");
+
 	/* TODO 1: request the keyboard I/O ports */
+	if (!request_region(I8042_DATA_REG, I8042_DATA_REG_SIZE, MODULE_NAME) ||
+	    !request_region(I8042_STAT_REG, I8042_STAT_REG_SIZE, MODULE_NAME)) {
+		err = -1;
+		goto out_release;
+	}
+
+	pr_info("[Success] request_region\n");
 
 	/* TODO 3: initialize spinlock */
+	spin_lock_init(&devs[0].lock);
 
 	/* TODO 2: Register IRQ handler for keyboard IRQ (IRQ 1). */
+	err = request_irq(I8042_KBD_IRQ, kbd_interrupt_handler, IRQF_SHARED,
+			  MODULE_NAME, &devs[0]);
+if (err < 0) {
+		pr_err("[Fail] request_irq returned %d\n", err);
+		goto out_unregister;
+	}
+
+	pr_info("[Success] request_irq\n");
 
 	cdev_init(&devs[0].cdev, &kbd_fops);
-	cdev_add(&devs[0].cdev, MKDEV(KBD_MAJOR, KBD_MINOR), 1);
+	cdev_add(&devs[0].cdev, dev, 1);
 
 	pr_notice("Driver %s loaded\n", MODULE_NAME);
 	return 0;
 
 	/*TODO 2: release regions in case of error */
+out_release:
+	pr_info("release_region\n");
+	release_region(I8042_DATA_REG, I8042_DATA_REG_SIZE);
+	release_region(I8042_STAT_REG, I8042_DATA_REG_SIZE);
 
 out_unregister:
-	unregister_chrdev_region(MKDEV(KBD_MAJOR, KBD_MINOR),
-				 KBD_NR_MINORS);
+	pr_info("unregister_chrdev_region\n");
+	free_irq(I8042_KBD_IRQ, &devs[0]);
+	unregister_chrdev_region(dev, KBD_NR_MINORS);
+
 out:
 	return err;
 }
@@ -177,12 +264,13 @@ static void kbd_exit(void)
 	cdev_del(&devs[0].cdev);
 
 	/* TODO 2: Free IRQ. */
+	free_irq(I8042_KBD_IRQ, &devs[0]);
 
 	/* TODO 1: release keyboard I/O ports */
+	release_region(I8042_DATA_REG, I8042_DATA_REG_SIZE);
+	release_region(I8042_STAT_REG, I8042_STAT_REG_SIZE);
 
-
-	unregister_chrdev_region(MKDEV(KBD_MAJOR, KBD_MINOR),
-				 KBD_NR_MINORS);
+	unregister_chrdev_region(MKDEV(KBD_MAJOR, KBD_MINOR), KBD_NR_MINORS);
 	pr_notice("Driver %s unloaded\n", MODULE_NAME);
 }
 
